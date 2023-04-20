4319e9f31c14d5ca11c78933ffd8d6be5ebf57da
Matt Hammond <mjhammond94@gmail.com>
2023-04-20 15:04:24 +0100

    further tests added - tests passed

--- spec/task_list_spec.rb
+++ spec/task_list_spec.rb
@@ -13,6 +13,17 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  it 'Should return a list of all tasks after multiple adds' do
+    task_list = TaskList.new
+    task = :one
+    task_1 = :two
+    task_2 = :three
+    task_list.add(task)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task, task_1, task_2]
+  end
+
   it "returns true if all tasks are complete" do
     task_list = TaskList.new
     complete_task = double(:task, complete?: true)
@@ -24,5 +35,14 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq true
   end
 
-  # false
+  it 'Should return false when one task has not been marked as complete' do
+    task_list = TaskList.new
+    complete_task = double :task, complete?: true
+    complete_task_1 = double :task, complete?: false
+    complete_task_2 = double :task, complete?: true
+    task_list.add(complete_task)
+    task_list.add(complete_task_1)
+    task_list.add(complete_task_2)
+    expect(task_list.all_complete?).to eq false
+  end
 end
