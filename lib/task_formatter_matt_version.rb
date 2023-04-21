fb826aecfcf957b084685452632c3b8a8967120d
Matt Hammond <mjhammond94@gmail.com>
2023-04-21 12:25:48 +0100

    formatter test passed

--- lib/task_formatter_matt_version.rb
+++ lib/task_formatter_matt_version.rb
@@ -9,10 +9,5 @@ class TaskFormatter
     else
       "- [ ] #{@task.title}"
     end
-    # Returns the task formatted as a string.
-    # If the task is not complete, the format is:
-    # - [ ] Task title
-    # If the task is complete, the format is:
-    # - [x] Task title
   end
 end
